6e95f299660af5fd043090b47014ec5f9535fb7a
jxp459 <juni1997parraga@gmail.com>
2020-10-26 16:32:37 -0500

    primer commit

---
+++ Semana3JParraga/Login.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Semana3JParraga
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+      private async void btnIngresar_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if(!txtUsuario.Text.Equals("estudiante2020")&& !txtClave.Text.Equals("uisrael2020")){
+                DisplayAlert("Alerta!", "El usuario y contraseña son incorrectos", "Cerrar");
+            }
+            else { 
+            if (!txtUsuario.Text.Equals("estudiante2020"))
+            {
+                DisplayAlert("Alerta!", "El usuario es incorrecto", "Cerrar");
+            }
+            else
+            {
+                    if (!txtClave.Text.Equals("uisrael2020"))
+                    {
+                        DisplayAlert("Alerta!", "La contraseña es incorrecta", "Cerrar");
+                    }
+                    else {
+                        await Navigation.PushAsync(new MainPage());
+                    }
+
+            }
+            }
+            
+        }
+
+    }
+}
---
+++ Semana3JParraga/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana3JParraga
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void btnSumar2_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                double Seguimiento2 = Convert.ToDouble(txtSeguimiento2.Text);
+                double Examen2 = Convert.ToDouble(txtExamen2.Text);
+                Seguimiento2 = Seguimiento2 * 0.3;
+                Examen2 = Examen2 * 0.2;
+                double suma = Seguimiento2 + Examen2;
+                txtResultado2.Text = suma.ToString();
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de alerta", "ERROR" + ex.Message, "ok");
+            }
+        }
+        void btnSumar1_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                double Seguimiento1 = Convert.ToDouble(txtSeguimiento1.Text);
+                double Examen1 = Convert.ToDouble(txtExamen1.Text);
+                Seguimiento1 = Seguimiento1 * 0.3;
+                Examen1 = Examen1 * 0.2;
+                double suma = Seguimiento1 + Examen1;
+                txtResultado1.Text = suma.ToString();
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de alerta", "ERROR" + ex.Message, "ok");
+            }
+        }
+        void btnSumarTotal_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+            double Resultado1 = Convert.ToDouble(txtResultado1.Text);
+            double Resultado2 = Convert.ToDouble(txtResultado2.Text);
+            double ResultadoFinal = Resultado1 + Resultado2;
+            if (ResultadoFinal >= 7)
+                DisplayAlert("Mensaje de alerta", "La nota es: " + ResultadoFinal + " y está Aprobado", "Cerrar");
+            else
+                DisplayAlert("Mensaje de alerta", "La nota es: " + ResultadoFinal + " y está Reprobado", "Cerrar");
+
+        }
+    }
+}
